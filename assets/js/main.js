75edb11aa3cdb329a30314c21b947d690c889402
Thang <caominhthangtran@gmail.com>
2024-01-24 04:00:43 +0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,5 +9,54 @@
 
     };
     fullHeight();
+
+    var $wrapper = $('.main-wrapper');
+	var $pageWrapper = $('.page-wrapper');
+	var $slimScrolls = $('.slimscroll');
+    // Sidebar
+	
+	var Sidemenu = function() {
+		this.$menuItem = $('#sidebar-menu a');
+	};
+	
+	function init() {
+		var $this = Sidemenu;
+		$('#sidebar-menu a').on('click', function(e) {
+			if($(this).parent().hasClass('submenu')) {
+				e.preventDefault();
+			}
+			if(!$(this).hasClass('subdrop')) {
+				$('ul', $(this).parents('ul:first')).slideUp(350);
+				$('a', $(this).parents('ul:first')).removeClass('subdrop');
+				$(this).next('ul').slideDown(350);
+				$(this).addClass('subdrop');
+			} else if($(this).hasClass('subdrop')) {
+				$(this).removeClass('subdrop');
+				$(this).next('ul').slideUp(350);
+			}
+		});
+		$('#sidebar-menu ul li.submenu a.active').parents('li:last').children('a:first').addClass('active').trigger('click');
+	}
+	
+	// Sidebar Initiate
+	init();
+	
+	// Mobile menu sidebar overlay
+	
+	$('body').append('<div class="sidebar-overlay"></div>');
+	$(document).on('click', '#mobile_btn', function() {
+		$wrapper.toggleClass('slide-nav');
+		$('.sidebar-overlay').toggleClass('opened');
+		$('html').addClass('menu-opened');
+		return false;
+	});
+
+    // Sidebar overlay
+	
+	$(".sidebar-overlay").on("click", function () {
+		$wrapper.removeClass('slide-nav');
+		$(".sidebar-overlay").removeClass("opened");
+		$('html').removeClass('menu-opened');
+	});
 })(jQuery);
 
